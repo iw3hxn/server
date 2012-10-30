36845cff797373c78f1e0089c93ca7ba1dfc6753
Vo Minh Thu <vmt@openerp.com>
2012-10-30 15:59:29 +0100

    [FIX] tests: install the french translation during test_translation_import.

--- openerp/tests/addons/test_translation_import/tests/test_term_count.py
+++ openerp/tests/addons/test_translation_import/tests/test_term_count.py
@@ -1,24 +1,16 @@
-import os
-import unittest2
+# -*- coding: utf-8 -*-
 
 import openerp
+from openerp.tests import common
 
-UID = openerp.SUPERUSER_ID
-DB = openerp.tools.config['db_name']
-
-class TestTermCount(unittest2.TestCase):
-
-    def setUp(self):
-        self.cr = openerp.modules.registry.RegistryManager.get(DB).db.cursor()
-        self.translation = openerp.modules.registry.RegistryManager.get(DB)['ir.translation']
-
-    def tearDown(self):
-        self.cr.rollback()
-        self.cr.close()
+class TestTermCount(common.TransactionCase):
 
     def test_count_term(self):
-        "Just make sure we have as many entries as we wanted."
-        ids = self.translation.search(self.cr, UID,
+        """
+        Just make sure we have as many translation entries as we wanted.
+        """
+        openerp.tools.trans_load(self.cr, 'test_translation_import/i18n/fr.po', 'fr_FR', verbose=False)
+        ids = self.registry('ir.translation').search(self.cr, self.uid,
             [('src', '=', '1XBUO5PUYH2RYZSA1FTLRYS8SPCNU1UYXMEYMM25ASV7JC2KTJZQESZYRV9L8CGB')])
         self.assertEqual(len(ids), 2)
 
